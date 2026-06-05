743a58fbd259d973205440583b697ec02a04662a
zye <1227976649@qe.com>
2026-06-05 17:31:10 +0800

    fix: MkSignin input cleanup (auto-commit by Hermes)

--- packages/frontend/src/components/MkSignin.input.vue
+++ packages/frontend/src/components/MkSignin.input.vue
@@ -39,15 +39,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 			<MkButton type="submit" large primary rounded style="margin: 0 auto;" data-cy-signin-page-input-continue>{{ i18n.ts.continue }} <i class="ti ti-arrow-right"></i></MkButton>
 		</form>
 
-		<!-- パスワードレスログイン -->
-		<div :class="$style.orHr">
-			<p :class="$style.orMsg">{{ i18n.ts.or }}</p>
-		</div>
-		<div>
-			<MkButton type="submit" style="margin: auto auto;" large rounded primary gradate @click="emit('passkeyClick', $event)">
-				<i class="ti ti-device-usb" style="font-size: medium;"></i>{{ i18n.ts.signinWithPasskey }}
-			</MkButton>
-		</div>
+		<!-- Passkey登录已隐藏（CG微米不需要） -->
 	</div>
 </div>
 </template>
